eab270e7edd01a775aa7a9e132ca91506fdf7299
manojpanta <manojpanta95@gmail.com>
2019-03-18 14:26:17 -0600

    fixing

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe InvoiceItem, type: :model do
     it {should belong_to :invoice }
     it {should belong_to :invoice }
     it {should belong_to :invoice }
+    it {should belong_to :invoice }
+    it {should belong_to :invoice }
   end
 end
