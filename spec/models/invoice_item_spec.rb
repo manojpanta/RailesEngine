0807a96e71a232175242e75af26e9d9d56e21010
manojpanta <manojpanta95@gmail.com>
2019-03-18 14:13:44 -0600

    added more validations test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it {should belong_to :item }
     it {should belong_to :invoice }
+    it {should belong_to :invoice }
+    it {should belong_to :invoice }
+    it {should belong_to :invoice }
   end
 end
